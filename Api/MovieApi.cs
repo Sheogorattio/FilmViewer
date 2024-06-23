25432967d8aa17e48996487d317f7f072bb7a687
Sheogorattio <91422100+Sheogorattio@users.noreply.github.com>
2024-06-23 18:45:28 +0300

    Add project files.

---
+++ Api/MovieApi.cs
@@ -0,0 +1,64 @@
+﻿using FilmViewer.Models;
+
+namespace FilmViewer.Api
+{
+    public class MovieApi
+    {
+        public static async Task<Movies> GetPopularMovies(int page = 1)
+        {
+            return await SendApi<Movies>($"https://api.themoviedb.org/3/movie/popular?language=ru-ru&page={page}&include_adult=true");
+        }
+        public static async Task<Movies> GetSimilarMovies(int movieId, int page = 1)
+        {
+            return await SendApi<Movies>($"https://api.themoviedb.org/3/movie/{movieId}/similar?&language=ru-ru&page={page}&include_adult=true");
+        }
+        public static async Task<Movies> GetMoviesByYearAndName(string name, int year, int page = 1)
+        {
+            return await SendApi<Movies>($"https://api.themoviedb.org/3/search/movie?language=ru-ru&query={name}&page={page}&include_adult=false&year={year}");
+        }
+        public static async Task<Movies> GetMoviesByName(string name, int page = 1)
+        {
+            return await SendApi<Movies>($"https://api.themoviedb.org/3/search/movie?language=ru-ru&query={name}&page={page}&include_adult=true");
+        }
+        public static async Task<Movies> GetMoviesByGenre(int genreId, int page = 1)
+        {
+            return await SendApi<Movies>($"https://api.themoviedb.org/3/discover/movie?with_genres={genreId}&page={page}&include_adult=true?language=ru-ru");
+        }
+        public static async Task<Movie> GetMovieById(int id)
+        {
+            return await SendApi<Movie>($"https://api.themoviedb.org/3/movie/{id}?language=ru-ru");
+        }
+        public static async Task<Genres> GetGenreList()
+        {
+            return await SendApi<Genres>("https://api.themoviedb.org/3/genre/movie/list?language=ru-ru");
+        }
+        private static async Task<T> SendApi<T>(string query)
+        {
+            string apiKey = GetMovieApiKey();
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage responseMessage = await httpClient.GetAsync(query + $"&api_key={apiKey}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var result = await responseMessage.Content.ReadFromJsonAsync<T>();
+                return result;
+            }
+            else
+            {
+                throw new Exception("Error. Try again later.");
+            }
+        }
+        private static string GetMovieApiKey()
+        {
+            var builder = new ConfigurationBuilder();
+            // установка пути к текущему каталогу
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            // получаем конфигурацию из файла appsettings.json
+            builder.AddJsonFile("appsettings.json");
+            // создаем конфигурацию
+            var config = builder.Build();
+            // получаем строку подключения
+            var connectionString = config.GetSection("MovieApi:ApiKey");
+            return connectionString.Value;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+global using FilmViewer.Api;
+global using FilmViewer.Models;
+using FilmViewer.Html;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+
+app.Map("/{page:int?}", async (HttpContext context, int page = 1) =>
+{
+    context.Response.ContentType = "text/html";
+
+    Movies movies = await MovieApi.GetPopularMovies(page);
+
+    return await HtmlBuilder.BuildPopularPage(movies, "");
+});
+
+app.Map("/Genre/{id:int}/{page:int?}", async (HttpContext context, int id, int page = 1) =>
+{
+    context.Response.ContentType = "text/html";
+
+    Movies movies = await MovieApi.GetMoviesByGenre(id, page);
+
+    return await HtmlBuilder.BuildPopularPage(movies, $"/Genre/{id}");
+});
+
+app.Map("/Search/{search}/{page:int?}", async (HttpContext context, string search, int page = 1) =>
+{
+    context.Response.ContentType = "text/html";
+
+    Movies movies = await MovieApi.GetMoviesByName(search, page);
+
+    return await HtmlBuilder.BuildPopularPage(movies, $"/Search/{search}");
+});
+
+app.Map("/Film/{id:int?}", async (HttpContext context, int id = 1) =>
+{
+    context.Response.ContentType = "text/html";
+
+    Movie movie = await MovieApi.GetMovieById(id);
+
+    return await HtmlBuilder.BuildFilmPage(movie);
+});
+
+app.Run();
